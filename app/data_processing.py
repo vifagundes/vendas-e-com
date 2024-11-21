9141ee6431ac06ec8f15bf2bd47d5f8ee30dfcda
vifagundes <viniblauthf@gmail.com>
2024-11-21 10:42:05 -0300

    corrigindo dockerfile e docker-compose

--- app/data_processing.py
+++ app/data_processing.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-df_ = pd.read_csv('./datasets/online_sales_dataset.csv')
-import pandas as pd
-
 def load_data(file_path):
     """Carrega os dados de um arquivo CSV."""
     return pd.read_csv(file_path)
